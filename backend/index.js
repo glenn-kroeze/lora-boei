381475c104489ce1df9df83d816b859d3afbd0b2
Glenn Kroeze <ggkroeze@gmail.com>
2019-09-30 21:49:00 +0200

    Use proper routing prefix for backend

--- backend/index.js
+++ backend/index.js
@@ -8,19 +8,22 @@ app.use(cors());
 const PORT = process.env.PORT || 4000;
 
 const { Device, Measurement } = require('./models');
+const router = express.Router();
 
-app.get('/api/devices', async (req, res) => {
+router.get('/devices', async (req, res) => {
   const devices = await Device.query()
     .orderBy('id');
   res.send(devices);
 });
 
-app.get('/api/measurements', async (req, res) => {
+router.get('/measurements', async (req, res) => {
   const id = req.query.deviceId;
   const measurements = await Measurement.query()
     .where({deviceId: id});
     res.send(measurements);
-})
+});
+
+app.use('/api', router);
 
 const Database = require('./Database');
 const database = new Database();
