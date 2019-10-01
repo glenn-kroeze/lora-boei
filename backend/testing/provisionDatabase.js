5ac2da5de9e25d0cbdd6e89fbcc800458b267e72
Glenn Kroeze <ggkroeze@gmail.com>
2019-10-01 11:02:48 +0200

    Improve database provisioning

--- backend/testing/provisionDatabase.js
+++ backend/testing/provisionDatabase.js
@@ -20,4 +20,44 @@ module.exports = async () => {
             },
         ]
       });
+
+      const device2 = await Device.query().insertGraph({
+        name: 'Tom',
+        location: JSON.stringify({lat: 51.9004892, lng: 4.419557}),
+        createdAt: new Date().toISOString(),
+        lastSeen: new Date().toISOString(),
+
+        measurements: [
+            {
+                waterTemperature: 12.5,
+                airTemperature: 22.3,
+                timeStamp: new Date().toISOString()
+            },
+            {
+                waterTemperature: 12.5,
+                airTemperature: 22.3,
+                timeStamp: new Date().toISOString()
+            },
+        ]
+      });
+
+      const device1 = await Device.query().insertGraph({
+        name: 'Kees',
+        location: JSON.stringify({lat: 51.8960132, lng: 4.4776482}),
+        createdAt: new Date().toISOString(),
+        lastSeen: new Date().toISOString(),
+
+        measurements: [
+            {
+                waterTemperature: 12.5,
+                airTemperature: 22.3,
+                timeStamp: new Date().toISOString()
+            },
+            {
+                waterTemperature: 12.5,
+                airTemperature: 22.3,
+                timeStamp: new Date().toISOString()
+            },
+        ]
+      });
 }
